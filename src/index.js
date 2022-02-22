394dd29d8be13e8c87e38acb4d9cd8f9e0383b42
josue recoba <josue.huer.reco.22@gamil.com>
2022-02-22 16:35:21 -0600

    Componete CounterApp

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraAPP';
 
+
 import './index.css';
+import CounterApp from './CounterAPP';
 
 
 const dicRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo  /> , divRoot);
+ReactDOM.render(<CounterApp value= {10}  /> , divRoot);
