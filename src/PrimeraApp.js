d912a7a8e65f10c18c755bb3a0e9e3cd8006c064
josue recoba <josue.huer.reco.22@gamil.com>
2022-02-22 15:42:18 -0600

    Reportar elementos en el componente -Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const PrimeraApp = () => {
 
-    return <h1>Hola Mundo </h1>;    
-  
+    return(
+        <>
+            <h1>Hola Mundo </h1>; 
+            <p>Mi primera aplicacion </p>
+        </>
+        
+    );
+    
+
 }
 
 
