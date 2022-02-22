0fff64730a3f546ea3d14bc61eac8153a86c46ed
josue recoba <josue.huer.reco.22@gamil.com>
2022-02-22 15:53:33 -0600

    Impresion de variables en el HTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const PrimeraApp = () => {
 
-    return(
+    const saludo = 'Hola Mundo';
+
+    return (
         <>
-            <h1>Hola Mundo </h1>; 
+            <h1> { saludo} </h1>
             <p>Mi primera aplicacion </p>
         </>
         
